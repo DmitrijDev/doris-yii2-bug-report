73efa55e20fb2e70095979be98f5f7a7d2a1d2f5
Dima K <d.dev@ukr.net>
2019-05-16 15:57:19 +0300

    Change gitignore and add id to user message

--- module/controllers/DefaultController.php
+++ module/controllers/DefaultController.php
@@ -8,6 +8,7 @@
 
 namespace doris\bugReport\module\controllers;
 
+use DateTime;
 use Yii;
 use Exception;
 use yii\web\Controller;
@@ -15,80 +16,83 @@ use doris\bugReport\module\helpers\ImageHelper;
 
 class DefaultController extends Controller
 {
-	public $hash = null;
-	public $email = null;
-	public $issueUrl = null;
+    public $hash = null;
+    public $email = null;
+    public $issueUrl = null;
 
-	public function init()
-	{
-		if (!isset(Yii::$app->params['bugReport']['apiKey'])) {
-			throw new Exception('ApiKey is empty');
-		}
+    public function init()
+    {
+        if (!isset(Yii::$app->params['bugReport']['apiKey'])) {
+            throw new Exception('ApiKey is empty');
+        }
 
-		if (!isset(Yii::$app->params['bugReport']['email'])) {
-			throw new Exception('User email is empty');
-		}
+        if (!isset(Yii::$app->params['bugReport']['email'])) {
+            throw new Exception('User email is empty');
+        }
 
-		if (!isset(Yii::$app->params['bugReport']['issueUrl'])) {
-			throw new Exception('Page url is empty');
-		}
+        if (!isset(Yii::$app->params['bugReport']['issueUrl'])) {
+            throw new Exception('Page url is empty');
+        }
 
-		$this->email = Yii::$app->params['bugReport']['email'];
-		$this->issueUrl = Yii::$app->params['bugReport']['issueUrl'];
-		$this->hash = md5($this->issueUrl . 'post_comment' . Yii::$app->params['bugReport']['apiKey']);
+        $this->email = Yii::$app->params['bugReport']['email'];
+        $this->issueUrl = Yii::$app->params['bugReport']['issueUrl'];
+        $this->hash = md5($this->issueUrl . 'post_comment' . Yii::$app->params['bugReport']['apiKey']);
 
-		parent::init();
-	}
+        parent::init();
+    }
 
-	public function actionIndex()
-	{
-		try {
-			$params = Yii::$app->request->post();
+    public function actionIndex()
+    {
+        try {
+            $params = Yii::$app->request->post();
 
-			$imageHelper = new ImageHelper();
-			$file = $imageHelper->saveImage($params['image']);
+            $imageHelper = new ImageHelper();
+            $file = $imageHelper->saveImage($params['image']);
+            $datetime = \DateTime::getTimestamp();
 
-			$text = implode('<br>', [
-				"Текст ошибки: {$params['message']}",
+            $text = implode('<br>', [
+                "id: ${$datetime}",
                 "----------------------------------",
-				"Ошибка на странице {$params['meta']['href']}",
-				"Размер экрана: {$params['meta']['viewportWidth']}x{$params['meta']['viewportHeight']}",
-				"Позиция по скролу: {$params['meta']['scrollX']}x{$params['meta']['scrollY']}",
-				"Данные об устройстве: {$params['meta']['device']}, {$params['meta']['os']}",
-				"Данные о браузере: {$params['meta']['browser']}, {$params['meta']['browserVersion']}",
-				"Мета: {$params['meta']['source']}"
-			]);
+                "Текст ошибки: {$params['message']}",
+                "----------------------------------",
+                "Ошибка на странице {$params['meta']['href']}",
+                "Размер экрана (WxY): {$params['meta']['viewportWidth']}x{$params['meta']['viewportHeight']}",
+                "Позиция по скролу (XxY): {$params['meta']['scrollX']}x{$params['meta']['scrollY']}",
+                "Данные об устройстве: {$params['meta']['device']}, {$params['meta']['os']}",
+                "Данные о браузере: {$params['meta']['browser']}, {$params['meta']['browserVersion']}",
+                "Мета: {$params['meta']['source']}"
+            ]);
 
-			$requestData = [
-				'action' => 'post_comment',
-				'page' => $this->issueUrl,
-				'email_user_from' => $this->email,
-				'text' => $text,
-				'hash' => $this->hash,
-				'todo[]' => 'Сделано!',
-				'attach[]' => new \CurlFile($file, 'image/png', 'Screenshot.png')
-			];
+            $requestData = [
+                'action' => 'post_comment',
+                'page' => $this->issueUrl,
+                'email_user_from' => $this->email,
+                'text' => $text,
+                'hash' => $this->hash,
+                'todo[]' => 'Сделано!',
+                'attach[]' => new \CurlFile($file, 'image/png', 'Screenshot.png')
+            ];
 
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, 'https://doris.worksection.com/api/admin/');
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $requestData);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			curl_setopt($ch, CURLOPT_HEADER, 1);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://doris.worksection.com/api/admin/');
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $requestData);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_HEADER, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-			$server_output = curl_exec($ch);
+            $server_output = curl_exec($ch);
 
-			curl_close($ch);
+            curl_close($ch);
 
-			if ($server_output == "OK") {
-				return 'OK';
-			} else {
-				return $server_output;
-			}
-		} catch (Exception $exception) {
-			return $exception->getMessage();
-		}
-	}
+            if ($server_output == "OK") {
+                return 'OK';
+            } else {
+                return $server_output;
+            }
+        } catch (Exception $exception) {
+            return $exception->getMessage();
+        }
+    }
 }
