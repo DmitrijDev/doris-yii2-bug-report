b59727f71028beca3f5ebe1015b319382f3cb249
Dima K <d.dev@ukr.net>
2019-07-09 10:16:09 +0300

    Basic worked structure

--- src/core/services/request-criteria.ts
+++ src/core/services/request-criteria.ts
@@ -1,95 +1,107 @@
-import * as _ from "lodash";
-
 export enum ORDER_TYPES {
-	DESC = 'desc',
-	ASC = 'asc'
+    DESC = 'desc',
+    ASC = 'asc'
 }
 
 export interface RequestCriteriaInterface {
-	perPage?: number;
-	expand?: String[];
-	page?: number;
-	condition?: { [fieldName: string]: Array<number | string | boolean> | null };
-	mixins?: String[];
-	order?: {
-		field: string;
-		type: ORDER_TYPES;
-	} | 'random';
-	groupBy?: String[]
+    perPage?: number;
+    expand?: String[];
+    page?: number;
+    condition?: { [fieldName: string]: Array<number | string | boolean> | null };
+    mixins?: String[];
+    order?: {
+        field: string;
+        type: ORDER_TYPES;
+    } | 'random';
+    groupBy?: String[]
 }
 
 export class RequestCriteria implements RequestCriteriaInterface {
-	perPage?: number;
-	page?: number;
-	expand?: String[];
-	condition?: { [fieldName: string]: Array<number | string | boolean> };
-	mixins?: [];
-	order?: {
-		field: string;
-		type: ORDER_TYPES;
-	} | 'random';
-	groupBy?: String[];
+    perPage?: number;
+    page?: number;
+    expand?: String[];
+    condition?: { [fieldName: string]: Array<number | string | boolean> | null };
+    mixins?: String[];
+    order?: {
+        field: string;
+        type: ORDER_TYPES;
+    } | 'random';
+    groupBy?: String[];
 
-	constructor(data: RequestCriteriaInterface) {
-		_.each(data, (value: any, field: string) => {
-			this.setProp(field, value);
-		});
-	}
+    constructor(data: RequestCriteriaInterface) {
+        if (data.perPage) {
+            this.perPage = data.perPage;
+        }
+        if (data.page) {
+            this.page = data.page;
+        }
+        if (data.expand) {
+            this.expand = data.expand;
+        }
+        if (data.condition) {
+            this.condition = data.condition;
+        }
+        if (data.mixins) {
+            this.mixins = data.mixins;
+        }
+        if (data.order) {
+            this.order = data.order;
+        }
+        if (data.groupBy) {
+            this.groupBy = data.groupBy;
+        }
+    }
 
-	setProp(name: string, value: any) {
-		if (_.isUndefined(value)) {
-			return;
-		}
+    getProps() {
+        let props = {
+            expand: '',
+            page: 0,
+            mixins: '',
+            groupBy: '',
+            perPage: 0,
+            sort: '',
+            filter: {}
+        };
 
-		_.set(this, name, value);
-	}
+        if (this.expand) {
+            props.expand = this.expand.join(',');
+        }
 
-	getProps() {
-		let props = {};
+        if (this.page) {
+            props.page = this.page;
+        }
 
-		if (!_.isUndefined(this.expand)) {
-			_.set(props, 'expand', this.expand.join(','));
-		}
+        if (this.mixins) {
+            props.mixins = this.mixins.join(',');
+        }
 
-		if (!_.isUndefined(this.page)) {
-			_.set(props, 'page', this.page);
-		}
+        if (this.groupBy) {
+            props.groupBy = this.groupBy.join(',');
+        }
 
-		if (!_.isUndefined(this.mixins)) {
-			_.set(props, 'mixins', this.mixins.join(','));
-		}
+        if (this.perPage) {
+            props.perPage = this.perPage;
+        }
 
-		if (!_.isUndefined(this.groupBy)) {
-			_.set(props, 'groupBy', this.groupBy.join(','));
-		}
+        if (this.order) {
+            let value = '';
 
-		if (!_.isUndefined(this.perPage)) {
-			_.set(props, 'per-page', this.perPage);
-		}
 
-		if (!_.isUndefined(this.order)) {
-			let value = '';
+            if (this.order === 'random') {
+                value = this.order;
+            } else {
+                let sign = this.order.type === ORDER_TYPES.ASC ? '' : '-';
+                value = sign + this.order.field
+            }
 
-			if (this.order === 'random') {
-				value = this.order;
-			} else {
-				let sign = this.order.type === ORDER_TYPES.ASC ? '' : '-';
-				value = sign + this.order.field
-			}
+            props.sort = value;
+        }
 
-			_.set(props, 'sort', value);
-		}
 
-		if (this.condition && Object.keys(this.condition).length) {
-			let condition = {};
+        if (this.condition && Object.keys(this.condition).length) {
+            props.filter = this.condition;
+        }
 
-			_.each(this.condition, (value: Array<string | number | boolean>, field: string) => {
-				_.set(condition, field, value);
-			});
-
-			_.set(props, 'filter', condition);
-		}
-
-		return props;
-	}
+        return props;
+    }
 }
